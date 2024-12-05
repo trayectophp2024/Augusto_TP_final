072bc3082613ee7f8b587500c89d3a4b4d35264f
Aucho98 <augustolorenzov@gmail.com>
2024-12-05 14:18:10 -0300

    detalles

--- views/alumno.php
+++ views/alumno.php
@@ -16,6 +16,21 @@
             <li class="list-group-item fs-5">Estilo de juego : Señuelo</li>
             <li class="list-group-item text-primary fs-5">Nacionalidad: Argentina</li>
             <li class="list-group-item text-success fs-5">Altura y Peso : 171 cm, 50 kg</li>
-            <li class="list-group-item text-warning fs-5">Ultimo Fichaje : Bayer Munich</li>
+            <li class="list-group-item text-warning fs-5">Ultimo Fichaje : Inter de Milan</li>
         </ul>
+
+        
+
     </div>
+</div>
+
+<div class="container ">
+            <h1 class="text-center text-white">Jugada</h1>
+        </div>
+
+        <div class="d-flex justify-content-center p-5 mt-3">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/R3MulHM-Xvs?si=-eVun9TbdMlZYGdx" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+        </div>
+
+            
--- views/nosotros.php
+++ views/nosotros.php
@@ -6,13 +6,13 @@
             <img width="500" class="d-block mx-auto img-fluid" src="img/logo.jpg" alt="nosotros">
         </div>
         <div class="col-12">
-            <p class="fs-4 text-center text-dark">Somos una empresa.</p>
+            <p class="fs-4 text-center text-white">Somos una pagina que disfruta ampliar y dar el mayor conocimiento del futbol, seguramente alguna vez visitaste una pagina del mismo tema el cual notaste que faltó informacion o actualizar la misma informacion. en la nuestra no pasara eso, siempre estamos dia al dia.</p>
         </div>
 
     </div>
 
     <div class="d-flex justify-content-center p-3">
-         <a class="fs-3 fw-bold btn btn-danger" href="index.php?sec=alumno">Perfil</a>
+         <a class="fs-3 fw-bold btn btn-danger" href="index.php?sec=alumno">Programador</a>
     </div>
 
     </div>
